11c05033fa6d8139a6f7de6d7d1ab8e3c2c3c2c6
yunji kim <yvnji25@gmail.com>
2023-03-09 23:53:39 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,7 +1,3 @@
 function solution(numbers, num1, num2) {
-    var answer = [];
-    for(let j = num1; j <= num2; j++) {
-        answer.push(numbers[j]);
-    }
-    return answer;
+    return numbers.slice(num1, num2+1);
 }
