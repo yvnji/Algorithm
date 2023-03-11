71fab4d3a32baae007ddaeb6c91e69359824406f
yunji kim <yvnji25@gmail.com>
2023-03-11 21:12:10 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,3 @@
 function solution(array, n) {
-    var answer = 0;
-    for(i in array) {
-        if(array[i]===n) {
-            answer += 1;
-        }
-    }
-    return answer;
+    return array.filter(v => v === n).length;
 }
