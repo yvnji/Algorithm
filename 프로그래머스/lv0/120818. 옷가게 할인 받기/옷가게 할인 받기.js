a63d134ee6a29c7b17ca4dd97fff094382f86863
yunji kim <yvnji25@gmail.com>
2023-03-19 23:47:41 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+    var answer = 0;
+    if(price>=100000 && 300000>price) {
+        answer += price * 0.95;
+    } else if(price>=300000 && 500000>price) {
+        answer += price * 0.9;
+    } else if(price>=500000) {
+        answer += price * 0.8;
+    } else { answer += price }
+    return Math.floor(answer);
+}
