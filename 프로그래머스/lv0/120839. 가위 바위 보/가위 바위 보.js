242d722f667c066a68e2169203fe77da05d2ffea
yunji kim <yvnji25@gmail.com>
2023-03-23 18:57:37 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+    var answer = '';
+    for(let i of rsp) {
+        if(i === '2') answer += '0'
+        else if(i === '0') answer += '5'
+        else answer += '2'
+    }
+    return answer;
+}
