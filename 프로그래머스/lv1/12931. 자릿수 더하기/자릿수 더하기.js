1b8b31328b174ce544b6519d5273b197edf1d4cd
yunji kim <yvnji25@gmail.com>
2023-04-02 00:32:35 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,3 @@
 function solution(n){
-    var numStr = n + '';
-    if(n>9) {
-        return numStr.split('').reduce((a, b)=> parseInt(a) + parseInt(b))      
-    } else {
-        return n
-    }
+    return (n+"").split("").reduce((a, b) => a + parseInt(b), 0)
 }
