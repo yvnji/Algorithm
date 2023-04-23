3abc74cd5debb153ae43f24addba92620f7ab024
yvnji <yvnji25@gmail.com>
2023-04-24 05:46:47 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,7 +1,3 @@
 function solution(s) {
-    return s.split('').sort(function(a, b) {
-          if(a < b) return 1;
-          if(a > b) return -1;
-          if(a === b) return 0;
-    }).join('');
+    return s.split('').sort().reverse().join('');
 }
