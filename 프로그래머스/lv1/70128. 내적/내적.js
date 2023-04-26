797faec03a788bd9488f3bad70a48853290011c8
yvnji <yvnji25@gmail.com>
2023-04-27 04:20:35 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    let answer = 0;
-    for(let i = 0; i <= a.length - 1; i++) {
-        answer += a[i]*b[i];      
-    }
-    return answer;
+    return a.reduce((sum, _, i) => sum += a[i]*b[i], 0);
 }
