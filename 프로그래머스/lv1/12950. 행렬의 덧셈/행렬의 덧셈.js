b75213cbdb1d5717a47bbb503c935d87f5936064
yvnji <yvnji25@gmail.com>
2023-05-11 23:48:51 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.49 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,15 +1,3 @@
-function solution(arr1, arr2) {
-    const rows = arr1.length;
-    const cols = arr1[0].length;
-    const answer = [];
-
-    for(let i = 0; i < rows; i++) {
-        const row = [];
-        for(let j = 0; j < cols; j++) {
-            row.push(arr1[i][j] + arr2[i][j]);
-        }
-        answer.push(row);
-    }
-
-    return answer;
+function solution(arr1,arr2){
+    return arr1.map((arr1, idx1) => arr1.map((val, idx2) => val+arr2[idx1][idx2]));
 }
